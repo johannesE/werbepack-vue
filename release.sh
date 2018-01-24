4ffe2d1430400cd70c0f5b1b550dbf449696dac4
Joe <github@octopusai.com>
2018-01-24 16:16:30 +0100

    Fix typo in release script

--- release.sh
+++ release.sh
@@ -4,6 +4,8 @@ git add .
 git status
 read -p "Enter commit message: " message
 git commit -m "$message"
+git push origin master
+sleep 1
 ssh -A root@werbepack.ch 'bash -s' <<ENDSSH
   echo pulling...
   cd /var/www/wp-content/plugins/werbepack-vue/
